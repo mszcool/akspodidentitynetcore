327f132c57f63e735749ceefa8e74367491ac749
Mario Szpuszta <marioszp@microsoft.com>
2021-01-03 22:38:24 +0100

    Moving devcert secret to user secrets

--- src/devcerts/generate.ps1
+++ src/devcerts/generate.ps1
@@ -1,3 +1,12 @@
+Param (
+    [String]
+    [Parameter(Mandatory=$true)]
+    $devCertPwd,
+
+    [Switch]
+    $updateSecrets
+)
+
 #
 # Generates certificates for local testing with docker compose and containers.
 # and includes the "service names" as alternative DNS names supported by the certificate which
@@ -8,16 +17,37 @@
 # - https://fearofoblivion.com/setting-up-asp-net-dev-certs-for-wsl
 # Both hints combined where needed, the 2nd article alone above did not help.
 #
-# Note: after generating new certs, make sure to wait 1-2 min before testing as the start time might be too early.
+# Note: after generating new certs, you need to wait until the cert becomes valid as the container runs in UTC (not your time zone, eventually).
 #
-rm akspodiddevcertwithservicenames.*
+Remove-Item -Force akspodiddevcertwithservicenames.*
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/CN=localhost" -config localhost.conf -keyout akspodiddevcertwithservicenames.key -out akspodiddevcertwithservicenames.crt
-openssl pkcs12 -export -in .\akspodiddevcertwithservicenames.crt -inkey .\akspodiddevcertwithservicenames.key -out .\akspodiddevcertwithservicenames.pfx -passout pass:dev_pwd_1
+openssl pkcs12 -export -in .\akspodiddevcertwithservicenames.crt -inkey .\akspodiddevcertwithservicenames.key -out .\akspodiddevcertwithservicenames.pfx -passout pass:$devCertPwd
 
 # Note: for this to work, the extension
 #       1.3.6.1.4.1.311.84.1.1 = ASN1:INTEGER:02 needs to be part of the certificate generated with openssl.
-dotnet dev-certs https --clean --import .\akspodiddevcertwithservicenames.pfx -p dev_pwd_1
+dotnet dev-certs https --clean --import .\akspodiddevcertwithservicenames.pfx -p $devCertPwd
+dotnet dev-certs https --trust
+
+#
+# Update user secrets
+#
+if ( $updateSecrets ) {
+
+    Copy-Item -Force ./akspodiddevcertwithservicenames.pfx ~/AppData/Roaming/ASP.NET/Https/MszCool.Samples.PodIdentityDemo.ResourcesFrontend.pfx
+    Copy-Item -Force ./akspodiddevcertwithservicenames.pfx ~/AppData/Roaming/ASP.NET/Https/MszCool.Samples.PodIdentityDemo.ResourcesBackend.pfx
+
+    Set-Location ..\ResourcesBackend
+    dotnet user-secrets set Kestrel:Certificates:Default:Password $devCertPwd
+    dotnet user-secrets set Kestrel:Certificates:Development:Password $devCertPwd
+
+    Set-Location ..\ResourcesFrontend
+    dotnet user-secrets set Kestrel:Certificates:Default:Password $devCertPwd
+    dotnet user-secrets set Kestrel:Certificates:Development:Password $devCertPwd
+
+    Set-Location ..\devcerts
+
+}
 
 #
 # Note: generating the certs with dotnet dev-certs https, exporting, converting to crt and using
@@ -27,8 +57,8 @@ dotnet dev-certs https --clean --import .\akspodiddevcertwithservicenames.pfx -p
 # Generate dev-certs for local debugging without compose
 #
 #dotnet dev-certs https --clean
-#dotnet dev-certs https -ep ./akspodiddevcertwithservicenames.pfx -p dev_pwd_1
+#dotnet dev-certs https -ep ./akspodiddevcertwithservicenames.pfx -p $devCertPwd
 
 # Convert the PFX so it works on Linux containers (Debian 10)
-#openssl pkcs12 -in akspodiddevcertwithservicenames.pfx -clcerts -nokeys -out akspodiddevcertwithservicenames.crt -passin pass:dev_pwd_1
-#openssl pkcs12 -in akspodiddevcertwithservicenames.pfx -nocerts -out akspodiddevcertwithservicenames.key -passin pass:dev_pwd_1 -passout pass:dev_pwd_1
+#openssl pkcs12 -in akspodiddevcertwithservicenames.pfx -clcerts -nokeys -out akspodiddevcertwithservicenames.crt -passin pass:$devCertPwd
+#openssl pkcs12 -in akspodiddevcertwithservicenames.pfx -nocerts -out akspodiddevcertwithservicenames.key -passin pass:$devCertPwd -passout pass:$devCertPwd
